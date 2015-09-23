491d52ae083508cb1862205033e4b0e1933d898f
Lcw <lcw2004@163.com>
2015-09-23 20:53:38 +0800

    封装Bean

--- src/invoice/dao/InvoiceDao.py
+++ src/invoice/dao/InvoiceDao.py
@@ -4,12 +4,12 @@ import sqlite3
 from BaseDao import BaseDao
 from BaseDao import appendSQL as appendSQL
 from invoice.common import config
+from invoice.bean.InvoiceBean import Invoice
 
 class InvoiceDao(BaseDao):
 
     def __init__(self):
         self.connect = sqlite3.connect(config.DATABASE_PATH)
-
 
     # 保存产品
     def save(self, invoice):
@@ -20,19 +20,18 @@ class InvoiceDao(BaseDao):
             '''
 
         cursor = self.connect.cursor()
-        cursor.execute(sql, [invoice["invoice_num"],
-                             invoice["custom_id"],
-                             invoice["remark"],
-                             invoice["start_time"],
-                             invoice["total_not_tax"],
-                             invoice["total_tax"],
-                             invoice["total_num"],
-                             invoice["serial_number"],
-                             invoice["drawer"],
-                             invoice["beneficiary"],
-                             invoice["reviewer"],
-                             invoice["status"]
-                             ])
+        cursor.execute(sql, [invoice.invoice_num,
+                             invoice.custom_id,
+                             invoice.remark,
+                             invoice.start_time,
+                             invoice.total_not_tax,
+                             invoice.total_tax,
+                             invoice.total_num,
+                             invoice.serial_number,
+                             invoice.drawer,
+                             invoice.beneficiary,
+                             invoice.reviewer,
+                             invoice.status])
         cursor.close()
         self.connect.commit()
 
@@ -44,22 +43,22 @@ class InvoiceDao(BaseDao):
         cursor.execute(sql)
 
         all = cursor.fetchall()
-        dicts = []
+        invoiceList = []
         for one in all:
-            dict = {}
-            dict["id"] = one[0]
-            dict["invoice_num"] = one[1]
-            dict["custom_id"] = one[2]
-            dict["remark"] = one[3]
-            dict["start_time"] = one[4]
-            dict["total_not_tax"] = one[5]
-            dict["total_tax"] = one[6]
-            dict["total_num"] = one[7]
-            dict["serial_number"] = one[8]
-            dict["drawer"] = one[9]
-            dict["beneficiary"] = one[10]
-            dict["reviewer"] = one[11]
-            dict["reviewer"] = one[12]
-            dicts.append(dict)
+            invoice = Invoice()
+            invoice.id = one[0]
+            invoice.invoice_num = one[1]
+            invoice.custom_id = one[2]
+            invoice.remark = one[3]
+            invoice.start_time = one[4]
+            invoice.total_not_tax = one[5]
+            invoice.total_tax = one[6]
+            invoice.total_num = one[7]
+            invoice.serial_number = one[8]
+            invoice.drawer = one[9]
+            invoice.beneficiary = one[10]
+            invoice.reviewer = one[11]
+            invoice.reviewer = one[12]
+            invoiceList.append(invoice)
         cursor.close()
-        return dicts
+        return invoiceList
--- src/test/dao/InvoiceDaoTest.py
+++ src/test/dao/InvoiceDaoTest.py
@@ -1,25 +1,24 @@
 # -*- coding: UTF-8 -*-
 from invoice.dao.InvoiceDao import InvoiceDao
+from invoice.bean.InvoiceBean import Invoice
 
 def testSave():
-    invoice = {}
-    invoice["invoice_num"] = "invoice_num"
-    invoice["custom_id"] = "custom_id"
-    invoice["remark"] = "remark"
-    invoice["start_time"] = "start_time"
-    invoice["total_not_tax"] = "total_not_tax"
-    invoice["total_tax"] = "total_tax"
-    invoice["total_num"] = "total_num"
-    invoice["serial_number"] = "serial_number"
-    invoice["drawer"] = "drawer"
-    invoice["beneficiary"] = "beneficiary"
-    invoice["reviewer"] = "reviewer"
-    invoice["status"] = 3
+    invoice = Invoice()
+    invoice.invoice_num = "invoice_num"
+    invoice.custom_id = "custom_id"
+    invoice.remark = "remark"
+    invoice.start_time = "start_time"
+    invoice.total_not_tax = "total_not_tax"
+    invoice.total_tax = "total_tax"
+    invoice.total_num = "total_num"
+    invoice.serial_number = "serial_number"
+    invoice.drawer = "drawer"
+    invoice.beneficiary = "beneficiary"
+    invoice.reviewer = "reviewer"
+    invoice.status = 3
 
     invoiceDao = InvoiceDao()
     invoiceDao.save(invoice)
-
-    pass
 
 def testGet():
     invoiceDao = InvoiceDao()
