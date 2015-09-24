c94ff42835785f5bf06481c9161e7d8eefd54500
Lcw <lcw2004@163.com>
2015-09-24 14:06:07 +0800

    简单实现导入到数据库中

--- src/invoice/gui/mainwindow.py
+++ src/invoice/gui/mainwindow.py
@@ -6,6 +6,18 @@ from PyQt4 import QtGui
 
 from mainwindow_ui import Ui_MainWindow
 from invoice.common import util
+from invoice.bean.InvoiceBean import Invoice
+from invoice.bean.InvoiceDetailBean import InvoiceDetail
+from invoice.bean.CustomBean import Custom
+from invoice.dao.InvoiceDao import InvoiceDao
+from invoice.dao.InvoiceDetailDao import InvoiceDetailDao
+from invoice.dao.CustomDao import CustomDao
+
+def qStringToString(input_str):
+    output_str = ""
+    if input_str:
+        output_str = unicode(input_str)
+    return output_str
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -30,21 +42,47 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             colCount = excelTableWidget.columnCount()
 
             for i in range(rowCount):
-                tbl_custom_name = excelTableWidget.item(i, 0).text()
-                tbl_invoice_invoice_num = excelTableWidget.item(i, 1).text()
-                tbl_invoice_total_not_tax = excelTableWidget.item(i, 2).text()
-                tbl_invoice_detail_pro_type = excelTableWidget.item(i, 3).text()
-                tbl_invoice_detail_pro_name = excelTableWidget.item(i, 4).text()
-                tbl_invoice_remark = excelTableWidget.item(i, 5).text()
+                tbl_custom_name = qStringToString(excelTableWidget.item(i, 0).text())
+                tbl_invoice_invoice_num = qStringToString(excelTableWidget.item(i, 1).text())
+                tbl_invoice_total_not_tax = qStringToString(excelTableWidget.item(i, 2).text())
+                tbl_invoice_detail_pro_type = qStringToString(excelTableWidget.item(i, 3).text())
+                tbl_invoice_detail_pro_name = qStringToString(excelTableWidget.item(i, 4).text())
+                tbl_invoice_remark = qStringToString(excelTableWidget.item(i, 5).text())
 
-                print tbl_custom_name
+                print type(tbl_custom_name)
                 print tbl_invoice_invoice_num
                 print tbl_invoice_total_not_tax
                 print tbl_invoice_detail_pro_type
                 print tbl_invoice_detail_pro_name
                 print tbl_invoice_remark
+
+                # 保存用户信息
+                custom = Custom()
+                custom.name = tbl_custom_name
+                cuntomDao = CustomDao()
+                cuntomDao.id = cuntomDao.save(custom)
+
+                # 保存发票
+                invoice = Invoice()
+                invoice.invoice_num = tbl_invoice_invoice_num
+                invoice.remark = tbl_invoice_remark
+                invoice.total_not_tax = tbl_invoice_total_not_tax
+                invoice.custom_id = custom.id
+
+                invoiceDao = InvoiceDao()
+                invoice.id = invoiceDao.save(invoice)
+
+                # 保存发票详细信息
+                invoiceDetail = InvoiceDetail()
+                invoiceDetail.pro_type = tbl_invoice_detail_pro_type
+                invoiceDetail.pro_name = tbl_invoice_detail_pro_name
+                invoiceDetail.invoice_Id = invoice.id
+
+                invoiceDetailDao = InvoiceDetailDao()
+                invoiceDetailDao.save(invoiceDetail)
                 print "-------------------------------"
 
             pass
 
         self.connect(self.genInvoiceButton, QtCore.SIGNAL("clicked()"), genInvoice)
+
