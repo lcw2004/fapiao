11ca8bbae002f2470cb2581a87198841d0de46b5
Lcw <lcw2004@163.com>
2015-09-24 16:20:34 +0800

    修改DictDao

--- src/invoice/dao/DictDao.py
+++ src/invoice/dao/DictDao.py
@@ -4,6 +4,7 @@ import sqlite3
 from BaseDao import BaseDao
 from BaseDao import appendSQL as appendSQL
 from invoice.common import config
+from invoice.bean.DictBean import Dict
 
 class DictDao(BaseDao):
 
@@ -12,7 +13,7 @@ class DictDao(BaseDao):
 
     # 根据产品名称查询
     def get(self, type=None, label=None):
-        sql = 'SELECT ID, label, value, type, describe, status, oindex FROM tbl_dict WHERE 1=1 '
+        sql = 'SELECT id, label, value, type, describe, status, oindex FROM tbl_dict WHERE 1=1 '
         sql += appendSQL("type", "=", type)
         sql += appendSQL("label", "=", label)
 
@@ -22,14 +23,14 @@ class DictDao(BaseDao):
         all = cursor.fetchall()
         dicts = []
         for one in all:
-            dict = {}
-            dict["ID"] = one[0]
-            dict["label"] = one[1]
-            dict["value"] = one[2]
-            dict["type"] = one[3]
-            dict["describe"] = one[4]
-            dict["status"] = one[5]
-            dict["oindex"] = one[6]
+            dict = Dict()
+            dict.id = one[0]
+            dict.label = one[1]
+            dict.value = one[2]
+            dict.type = one[3]
+            dict.describe = one[4]
+            dict.status = one[5]
+            dict.oindex = one[6]
             dicts.append(dict)
         cursor.close()
         return dicts
@@ -39,24 +40,20 @@ class DictDao(BaseDao):
         sql += appendSQL("type", "=", type)
         sql += appendSQL("label", "=", label)
 
-        print sql
-
         cursor = self.connect.cursor()
         cursor.execute(sql)
 
         all = cursor.fetchall()
         dicts = {}
         for one in all:
-            dict = {}
-            dict["ID"] = one[0]
-            dict["label"] = one[1]
-            dict["value"] = one[2]
-            dict["type"] = one[3]
-            dict["describe"] = one[4]
-            dict["status"] = one[5]
-            dict["oindex"] = one[6]
-            dicts[one[1]] = dict
+            dict = Dict()
+            dict.id = one[0]
+            dict.label = one[1]
+            dict.value = one[2]
+            dict.type = one[3]
+            dict.describe = one[4]
+            dict.status = one[5]
+            dict.oindex = one[6]
+            dicts[dict.label] = dict
         cursor.close()
         return dicts
-
-
--- src/test/dao/DictDaoTest.py
+++ src/test/dao/DictDaoTest.py
@@ -1,6 +1,34 @@
 # -*- coding: UTF-8 -*-
 from invoice.dao.DictDao import DictDao
+from invoice.bean.DictBean import Dict
+
+def testGet():
+    dictDao = DictDao()
+    list = dictDao.get(type="EXCEL_TO_XML")
+    for dict in list:
+        print "------------------------"
+        print dict.id
+        print dict.label
+        print dict.value
+        print dict.type
+        print dict.describe
+        print dict.status
+        print dict.oindex
+
+def testGetExcelConfig():
+    dictDao = DictDao()
+    list = dictDao.getExcelConfig(type="EXCEL_TO_XML")
+    for label in list:
+        print "------------------------"
+        dict = list[label]
+        print dict.id
+        print dict.label
+        print dict.value
+        print dict.type
+        print dict.describe
+        print dict.status
+        print dict.oindex
 
 if __name__ == "__main__":
-    dictDao = DictDao()
-    print dictDao.get(type="EXCEL_TO_XML")
+    testGet()
+    testGetExcelConfig()
