5853ee017eccd56055bbbc24d6df6e0e3a02dc01
Lcw <lcw2004@163.com>
2015-11-01 16:55:34 +0800

    重构对象名称

--- src/invoice/gui/mainwindow.py
+++ src/invoice/gui/mainwindow.py
@@ -4,6 +4,7 @@ from PyQt4.QtGui import QMainWindow, QMessageBox, QAbstractItemView
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 import peewee
+from invoice.gui.form_invoice import FormInvoiceDialog
 from mainwindow_ui import Ui_MainWindow
 
 from invoice.sys import ExportAsXML
@@ -19,43 +20,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # 绑定选择Excel事件
-        self.connect(self.selectExcelFileButton, QtCore.SIGNAL("clicked()"), self.selectExcel)
+        self.connect(self.excel_selectl_file_btn, QtCore.SIGNAL("clicked()"), self.excel_selectl_file_btn_clicked)
 
         # 数据导入 - 生成发票
-        self.connect(self.genInvoiceButton, QtCore.SIGNAL("clicked()"), self.genInvoice)
+        self.connect(self.excel_gen_invoice_btn, QtCore.SIGNAL("clicked()"), self.excel_gen_invoice_btn_clicked)
 
         # 临时待处理数据 - 表格加载
-        self.connect(self.invoice_filter_Button, QtCore.SIGNAL("clicked()"), self.queryInvoice)
+        self.connect(self.invoice_filter_btn, QtCore.SIGNAL("clicked()"), self.invoice_filter_btn_clicked)
 
         # 绑定元素选择事件
-        self.connect(self.invoiceTableWidget, QtCore.SIGNAL('itemClicked(QTableWidgetItem*)'), self.invoinceItem_Clicked)
+        self.connect(self.invoice_table, QtCore.SIGNAL('itemClicked(QTableWidgetItem*)'), self.invoice_table_item_clicked)
 
         # 临时待处理数据 - 修改
-        self.connect(self.invoince_update_btn, QtCore.SIGNAL("clicked()"), self.updateInvoince)
+        self.connect(self.invoince_update_btn, QtCore.SIGNAL("clicked()"), self.invoince_update_btn_clicked)
 
         # 临时待处理数据 - 删除
-        self.connect(self.invoince_delete_btn, QtCore.SIGNAL("clicked()"), self.on_action_Delete_Invoice)
+        self.connect(self.invoince_delete_btn, QtCore.SIGNAL("clicked()"), self.invoince_delete_btn_clicked)
 
         # 临时待处理数据 - 导入到开票系统
-        self.connect(self.invoince_import_xml_btn, QtCore.SIGNAL("clicked()"), self.exportInvoinceAsXml)
+        self.connect(self.invoince_import_xml_btn, QtCore.SIGNAL("clicked()"), self.invoince_import_xml_btn_clicked)
 
         # 临时待处理数据 - 合并选中项
-        self.connect(self.invoince_merge_btn, QtCore.SIGNAL("clicked()"), self.mergeInvoice)
+        self.connect(self.invoince_merge_btn, QtCore.SIGNAL("clicked()"), self.invoince_merge_btn_clicked)
 
         # 临时待处理数据 - 合并发票相同的产品
-        self.connect(self.invoince_merge_product_btn, QtCore.SIGNAL("clicked()"), self.mergeInvoiceDetail)
+        self.connect(self.invoince_merge_product_btn, QtCore.SIGNAL("clicked()"), self.invoince_merge_product_btn_clicked)
 
         # 临时待处理数据 - 拆分(按最大限额)
-        self.connect(self.invoince_chaifeng_btn, QtCore.SIGNAL("clicked()"), self.chaifenInvoice)
+        self.connect(self.invoince_chaifeng_btn, QtCore.SIGNAL("clicked()"), self.invoince_chaifeng_btn_clicked)
 
-    def mergeInvoiceDetail(self):
+    def invoince_merge_product_btn_clicked(self):
         pass
 
-    def mergeInvoice(self):
-        invoiceTableWidget = self.invoiceTableWidget
+    def invoince_merge_btn_clicked(self):
+        invoice_table = self.invoice_table
 
         # 判断选择的合并的发票数量
-        selected_rows = tableUtil.getSelectedRows(invoiceTableWidget)
+        selected_rows = tableUtil.getSelectedRows(invoice_table)
         if len(selected_rows) <= 1:
             QMessageBox.information(self, "Information", u'请选择至少两个需要合并的发票！')
             return
@@ -64,8 +65,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         idList = []
         customNameList = []
         for row_num in selected_rows:
-            invoince_id = tableUtil.qStringToString(invoiceTableWidget.item(row_num, 0).text())
-            invoince_custom_name = tableUtil.qStringToString(invoiceTableWidget.item(row_num, 2).text())
+            invoince_id = tableUtil.qStringToString(invoice_table.item(row_num, 0).text())
+            invoince_custom_name = tableUtil.qStringToString(invoice_table.item(row_num, 2).text())
             if invoince_id:
                 idList.append(int(invoince_id))
                 customNameList.append(invoince_custom_name)
@@ -89,14 +90,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         # 合并成功并刷新表格
         QMessageBox.information(self, "Information", u'合并成功！')
-        self.queryInvoice()
+        self.invoice_filter_btn_clicked()
 
 
-    def chaifenInvoice(self):
-        invoinceDetailTableWidget = self.invoinceDetailTableWidget
+    def invoince_chaifeng_btn_clicked(self):
+        invoice_detail_table = self.invoice_detail_table
 
         # 判断选择的合并的发票数量
-        selected_rows = tableUtil.getSelectedRows(invoinceDetailTableWidget)
+        selected_rows = tableUtil.getSelectedRows(invoice_detail_table)
         if len(selected_rows) < 1:
             QMessageBox.information(self, "Information", u'请选择至少一个需要拆分的发票明细！')
             return
@@ -104,7 +105,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # 获取所有需要合并的行的ID
         idList = []
         for row_num in selected_rows:
-            invoince_id = tableUtil.qStringToString(invoinceDetailTableWidget.item(row_num, 0).text())
+            invoince_id = tableUtil.qStringToString(invoice_detail_table.item(row_num, 0).text())
             if invoince_id:
                 idList.append(int(invoince_id))
 
@@ -128,20 +129,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             # invoiceDao.proofreadInvoince(newInvoiceId)
             # invoiceDao.proofreadInvoince(oldInvoiceId)
             QMessageBox.information(self, "Information", u'拆分成功！')
-            self.queryInvoice()
+            self.invoice_filter_btn_clicked()
 
         pass
 
-    def selectExcel(self):
+    def excel_selectl_file_btn_clicked(self):
         # TODO 判断是否选择文件
         filename = QtGui.QFileDialog.getOpenFileName(self, 'Excel', '../', 'Excel File (*.xls)')
         if filename:
-            excelparse.parseExcel(filename, self.excelTableWidget)
+            excelparse.parseExcel(filename, self.excel_table)
 
-    def genInvoice(self):
-        excelTableWidget = self.excelTableWidget
-        rowCount = excelTableWidget.rowCount()
-        colCount = excelTableWidget.columnCount()
+    def excel_gen_invoice_btn_clicked(self):
+        excel_table = self.excel_table
+        rowCount = excel_table.rowCount()
+        colCount = excel_table.columnCount()
 
         # TODO 判断表格中是否有数据
         if rowCount <= 1:
@@ -149,12 +150,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             return
 
         for i in range(rowCount):
-            tbl_custom_name = tableUtil.qStringToString(excelTableWidget.item(i, 0).text())
-            tbl_invoice_invoice_num = tableUtil.qStringToString(excelTableWidget.item(i, 1).text())
-            tbl_invoice_total_not_tax = tableUtil.qStringToString(excelTableWidget.item(i, 2).text())
-            tbl_invoice_detail_pro_type = tableUtil.qStringToString(excelTableWidget.item(i, 3).text())
-            tbl_invoice_detail_pro_name = tableUtil.qStringToString(excelTableWidget.item(i, 4).text())
-            tbl_invoice_remark = tableUtil.qStringToString(excelTableWidget.item(i, 5).text())
+            tbl_custom_name = tableUtil.qStringToString(excel_table.item(i, 0).text())
+            tbl_invoice_invoice_num = tableUtil.qStringToString(excel_table.item(i, 1).text())
+            tbl_invoice_total_not_tax = tableUtil.qStringToString(excel_table.item(i, 2).text())
+            tbl_invoice_detail_pro_type = tableUtil.qStringToString(excel_table.item(i, 3).text())
+            tbl_invoice_detail_pro_name = tableUtil.qStringToString(excel_table.item(i, 4).text())
+            tbl_invoice_remark = tableUtil.qStringToString(excel_table.item(i, 5).text())
 
             # 保存用户信息
             try:
@@ -203,65 +204,69 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         QMessageBox.information(self, "Information", u'数据已经保存到临时数据区！')
         # TODO 导入成功之后清空数据
 
-    def queryInvoice(self):
-        invoiceTableWidget = self.invoiceTableWidget
+    def invoice_filter_btn_clicked(self):
+        invoice_table = self.invoice_table
 
         # 设置整行选中
-        invoiceTableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        invoice_table.setSelectionBehavior(QAbstractItemView.SelectRows)
         # 设置不可编辑
-        invoiceTableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        invoice_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         # 查询数据
         invoiceList = list(Invoice.select().where(Invoice.status==0))
 
 
         rowCount = len(invoiceList)
-        self.invoiceTableWidget.setRowCount(rowCount)
+        self.invoice_table.setRowCount(rowCount)
 
         # 将数据加载到表格中
         for i in range(rowCount):
             invoice = invoiceList[i]
 
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 0, invoice.id)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 1, invoice.invoice_num)
+            tableUtil.setTableItemValue(invoice_table, i, 0, invoice.id)
+            tableUtil.setTableItemValue(invoice_table, i, 1, invoice.invoice_num)
             if invoice.custom:
-                tableUtil.setTableItemValue(invoiceTableWidget, i, 2, invoice.custom.name)
-                tableUtil.setTableItemValue(invoiceTableWidget, i, 7, invoice.custom.code)
-                tableUtil.setTableItemValue(invoiceTableWidget, i, 8, invoice.custom.tax_id)
-                tableUtil.setTableItemValue(invoiceTableWidget, i, 9, invoice.custom.addr)
-                tableUtil.setTableItemValue(invoiceTableWidget, i, 10, invoice.custom.bank_account)
+                tableUtil.setTableItemValue(invoice_table, i, 2, invoice.custom.name)
+                tableUtil.setTableItemValue(invoice_table, i, 7, invoice.custom.code)
+                tableUtil.setTableItemValue(invoice_table, i, 8, invoice.custom.tax_id)
+                tableUtil.setTableItemValue(invoice_table, i, 9, invoice.custom.addr)
+                tableUtil.setTableItemValue(invoice_table, i, 10, invoice.custom.bank_account)
 
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 3, invoice.total_not_tax)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 4, invoice.total_tax)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 5, invoice.total_num)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 6, invoice.serial_number)
+            tableUtil.setTableItemValue(invoice_table, i, 3, invoice.total_not_tax)
+            tableUtil.setTableItemValue(invoice_table, i, 4, invoice.total_tax)
+            tableUtil.setTableItemValue(invoice_table, i, 5, invoice.total_num)
+            tableUtil.setTableItemValue(invoice_table, i, 6, invoice.serial_number)
 
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 11, invoice.remark)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 12, invoice.drawer)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 13, invoice.beneficiary)
-            tableUtil.setTableItemValue(invoiceTableWidget, i, 14, invoice.reviewer)
+            tableUtil.setTableItemValue(invoice_table, i, 11, invoice.remark)
+            tableUtil.setTableItemValue(invoice_table, i, 12, invoice.drawer)
+            tableUtil.setTableItemValue(invoice_table, i, 13, invoice.beneficiary)
+            tableUtil.setTableItemValue(invoice_table, i, 14, invoice.reviewer)
 
-    def updateInvoince(self):
+    def invoince_update_btn_clicked(self):
+        dialog = FormInvoiceDialog(self)
+        dialog.show()
+
+
         pass
 
-    def on_action_Delete_Invoice(self):
+    def invoince_delete_btn_clicked(self):
             reply = QMessageBox.question(self,u'提示',u'确定要删除所选记录吗？',QMessageBox.Yes|QMessageBox.No)
             if reply == QMessageBox.Yes:
-                invoiceTableWidget = self.invoiceTableWidget
+                invoice_table = self.invoice_table
 
                 # 获取所有需要删除的行的ID
                 idList = []
-                remove_rows = tableUtil.getSelectedRows(invoiceTableWidget)
+                remove_rows = tableUtil.getSelectedRows(invoice_table)
                 for rowCount in remove_rows:
-                    invoince_id = tableUtil.qStringToString(invoiceTableWidget.item(rowCount, 0).text())
+                    invoince_id = tableUtil.qStringToString(invoice_table.item(rowCount, 0).text())
                     if invoince_id:
                         q = Invoice.update(status=-1).where(Invoice.id == invoince_id)
                         q.execute()
 
                 # 重新加载表格
-                self.queryInvoice()
+                self.invoice_filter_btn_clicked()
 
-    def exportInvoinceAsXml(self):
+    def invoince_import_xml_btn_clicked(self):
         invoinceList = list(Invoice.select(Invoice.status == 0))
         isSuccess = ExportAsXML.exportAsFile(invoinceList, "1.xml")
         if isSuccess:
@@ -269,37 +274,37 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             QMessageBox.information(self, "Information", u'导入失败，请重试！')
 
-    def invoinceItem_Clicked(self, item):
-        invoinceDetailTableWidget = self.invoinceDetailTableWidget
+    def invoice_table_item_clicked(self, item):
+        invoice_detail_table = self.invoice_detail_table
 
         # 设置整行选中
-        invoinceDetailTableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+        invoice_detail_table.setSelectionBehavior(QAbstractItemView.SelectRows)
 
         # 设置不可编辑
-        invoinceDetailTableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        invoice_detail_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         # 获取选中行的ID
-        invoiceId =  self.invoiceTableWidget.item(item.row(), 0).text()
+        invoiceId =  self.invoice_table.item(item.row(), 0).text()
 
         # 根据ID查询明细
         invoiceDetailList = list(Invoice.get(id=invoiceId).invoiceDetails)
 
         rowCount = len(invoiceDetailList)
-        self.invoinceDetailTableWidget.setRowCount(rowCount)
+        self.invoice_detail_table.setRowCount(rowCount)
 
         # 将数据加载到表格中
         for i in range(rowCount):
             invoiceDetail = invoiceDetailList[i]
 
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 0, invoiceDetail.id)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 1, invoiceDetail.product.code)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 2, invoiceDetail.product.name)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 3, invoiceDetail.product.type)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 4, invoiceDetail.product.unit)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 5, invoiceDetail.product.unit_price)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 6, invoiceDetail.pro_num)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 7, invoiceDetail.product.tax_price)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 8, invoiceDetail.not_tax_price)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 9, invoiceDetail.product.tax)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 10, invoiceDetail.tax_price)
-            tableUtil.setTableItemValue(invoinceDetailTableWidget, i, 11, invoiceDetail.contain_tax_price)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 0, invoiceDetail.id)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 1, invoiceDetail.product.code)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 2, invoiceDetail.product.name)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 3, invoiceDetail.product.type)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 4, invoiceDetail.product.unit)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 5, invoiceDetail.product.unit_price)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 6, invoiceDetail.pro_num)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 7, invoiceDetail.product.tax_price)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 8, invoiceDetail.not_tax_price)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 9, invoiceDetail.product.tax)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 10, invoiceDetail.tax_price)
+            tableUtil.setTableItemValue(invoice_detail_table, i, 11, invoiceDetail.contain_tax_price)
--- src/invoice/gui/ui2py.bat
+++ src/invoice/gui/ui2py.bat
@@ -1,1 +1,2 @@
 C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py mainwindow.ui > mainwindow_ui.py
+C:\Python27\Lib\site-packages\PyQt4\uic\pyuic.py form_invoice.ui > form_invoice_ui.py
