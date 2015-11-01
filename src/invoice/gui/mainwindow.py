646e2291c85b9958f4222e1a02bcd1d2a6f5ff9e
Lcw <lcw2004@163.com>
2015-11-01 17:58:26 +0800

    重构对象名称

--- src/invoice/gui/mainwindow.py
+++ src/invoice/gui/mainwindow.py
@@ -3,15 +3,14 @@
 from PyQt4.QtGui import QMainWindow, QMessageBox, QAbstractItemView
 from PyQt4 import QtCore
 from PyQt4 import QtGui
-import peewee
+
 from invoice.gui.form_invoice import FormInvoiceDialog
 from mainwindow_ui import Ui_MainWindow
-
 from invoice.sys import ExportAsXML
 from invoice.common import excelparse
 from invoice.common import tableUtil
-
 from invoice.bean.Beans import *
+
 
 class MainWindow(QMainWindow, Ui_MainWindow):
 
@@ -32,124 +31,124 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.connect(self.invoice_table, QtCore.SIGNAL('itemClicked(QTableWidgetItem*)'), self.invoice_table_item_clicked)
 
         # 临时待处理数据 - 修改
-        self.connect(self.invoince_update_btn, QtCore.SIGNAL("clicked()"), self.invoince_update_btn_clicked)
+        self.connect(self.invoine_update_btn, QtCore.SIGNAL("clicked()"), self.invoice_update_btn_clicked)
 
         # 临时待处理数据 - 删除
-        self.connect(self.invoince_delete_btn, QtCore.SIGNAL("clicked()"), self.invoince_delete_btn_clicked)
+        self.connect(self.invoice_delete_btn, QtCore.SIGNAL("clicked()"), self.invoice_delete_btn_clicked)
 
         # 临时待处理数据 - 导入到开票系统
-        self.connect(self.invoince_import_xml_btn, QtCore.SIGNAL("clicked()"), self.invoince_import_xml_btn_clicked)
+        self.connect(self.invoice_import_xml_btn, QtCore.SIGNAL("clicked()"), self.invoice_import_xml_btn_clicked)
 
         # 临时待处理数据 - 合并选中项
-        self.connect(self.invoince_merge_btn, QtCore.SIGNAL("clicked()"), self.invoince_merge_btn_clicked)
+        self.connect(self.invoice_merge_btn, QtCore.SIGNAL("clicked()"), self.invoice_merge_btn_clicked)
 
         # 临时待处理数据 - 合并发票相同的产品
-        self.connect(self.invoince_merge_product_btn, QtCore.SIGNAL("clicked()"), self.invoince_merge_product_btn_clicked)
+        self.connect(self.invoice_merge_product_btn, QtCore.SIGNAL("clicked()"), self.invoice_merge_product_btn_clicked)
 
         # 临时待处理数据 - 拆分(按最大限额)
-        self.connect(self.invoince_chaifeng_btn, QtCore.SIGNAL("clicked()"), self.invoince_chaifeng_btn_clicked)
+        self.connect(self.invoice_chaifeng_btn, QtCore.SIGNAL("clicked()"), self.invoice_chaifeng_btn_clicked)
 
-    def invoince_merge_product_btn_clicked(self):
+    def invoice_merge_product_btn_clicked(self):
         pass
 
-    def invoince_merge_btn_clicked(self):
+    def invoice_merge_btn_clicked(self):
         invoice_table = self.invoice_table
 
         # 判断选择的合并的发票数量
         selected_rows = tableUtil.getSelectedRows(invoice_table)
         if len(selected_rows) <= 1:
-            QMessageBox.information(self, "Information", u'请选择至少两个需要合并的发票！')
+            QMessageBox.information(None, "Information", u'请选择至少两个需要合并的发票！')
             return
 
         # 获取所有需要合并的行的ID
-        idList = []
-        customNameList = []
+        invoice_id_list = []
+        custom_name_list = []
         for row_num in selected_rows:
-            invoince_id = tableUtil.qStringToString(invoice_table.item(row_num, 0).text())
-            invoince_custom_name = tableUtil.qStringToString(invoice_table.item(row_num, 2).text())
-            if invoince_id:
-                idList.append(int(invoince_id))
-                customNameList.append(invoince_custom_name)
+            invoice_id = tableUtil.qStringToString(invoice_table.item(row_num, 0).text())
+            invoice_custom_name = tableUtil.qStringToString(invoice_table.item(row_num, 2).text())
+            if invoice_id:
+                invoice_id_list.append(int(invoice_id))
+                custom_name_list.append(invoice_custom_name)
 
         # 判断是否
-        customNameSet = set(customNameList)
+        customNameSet = set(custom_name_list)
         if len(customNameSet) > 1:
-            QMessageBox.information(self, "Information", u'所选发票中存在两个不同客户的发票！')
+            QMessageBox.information(None, "Information", u'所选发票中存在两个不同客户的发票！')
             return
 
         # 将所选发票的详细信息合并到第一个中，并删除其他发票
-        mainInvoiceId = idList[0]
-        mainInvoice = Invoice.get(id=mainInvoiceId)
-        q = InvoiceDetail.update(invoice=mainInvoice).where(InvoiceDetail.invoice << idList[1:])
+        main_invoice_id = invoice_id_list[0]
+        main_invoice = Invoice.get(id=main_invoice_id)
+        q = InvoiceDetail.update(invoice=main_invoice).where(InvoiceDetail.invoice << invoice_id_list[1:])
         q.execute()
-        q = Invoice.update(status=9).where(Invoice.id << idList[1:])
+        q = Invoice.update(status=9).where(Invoice.id << invoice_id_list[1:])
         q.execute()
 
         # TODO 重新统计税额
-        # invoiceDao.proofreadInvoince(mainInvoiceId)
+        # invoiceDao.proofreadInvoince(main_invoice_id)
 
         # 合并成功并刷新表格
-        QMessageBox.information(self, "Information", u'合并成功！')
+        QMessageBox.information(None, "Information", u'合并成功！')
         self.invoice_filter_btn_clicked()
 
 
-    def invoince_chaifeng_btn_clicked(self):
+    def invoice_chaifeng_btn_clicked(self):
         invoice_detail_table = self.invoice_detail_table
 
         # 判断选择的合并的发票数量
         selected_rows = tableUtil.getSelectedRows(invoice_detail_table)
         if len(selected_rows) < 1:
-            QMessageBox.information(self, "Information", u'请选择至少一个需要拆分的发票明细！')
+            QMessageBox.information(None, "Information", u'请选择至少一个需要拆分的发票明细！')
             return
 
         # 获取所有需要合并的行的ID
-        idList = []
+        invoice_id_list = []
         for row_num in selected_rows:
-            invoince_id = tableUtil.qStringToString(invoice_detail_table.item(row_num, 0).text())
-            if invoince_id:
-                idList.append(int(invoince_id))
+            invoice_id = tableUtil.qStringToString(invoice_detail_table.item(row_num, 0).text())
+            if invoice_id:
+                invoice_id_list.append(int(invoice_id))
 
         # 获取发票ID
-        if idList:
-            print idList
-            invoiceDetail = InvoiceDetail.get(id=idList[0])
-            invoice = invoiceDetail.invoice
+        if invoice_id_list:
+            print invoice_id_list
+            invoice_detail = InvoiceDetail.get(id=invoice_id_list[0])
+            invoice = invoice_detail.invoice
 
-            newInvoice = Invoice.create(invoice_num=invoice.invoice_num,
+            new_invoice = Invoice.create(invoice_num=invoice.invoice_num,
                                      remark=invoice.remark,
                                      total_not_tax=invoice.total_not_tax,
                                      custom=invoice.custom)
-            newInvoice.save()
+            new_invoice.save()
 
-            q = InvoiceDetail.update(invoice=newInvoice).where(InvoiceDetail.id << idList)
+            q = InvoiceDetail.update(invoice=new_invoice).where(InvoiceDetail.id << invoice_id_list)
             print q
             q.execute()
 
             # TODO 重新统计税额
             # invoiceDao.proofreadInvoince(newInvoiceId)
             # invoiceDao.proofreadInvoince(oldInvoiceId)
-            QMessageBox.information(self, "Information", u'拆分成功！')
+            QMessageBox.information(None, "Information", u'拆分成功！')
             self.invoice_filter_btn_clicked()
 
         pass
 
     def excel_selectl_file_btn_clicked(self):
         # TODO 判断是否选择文件
-        filename = QtGui.QFileDialog.getOpenFileName(self, 'Excel', '../', 'Excel File (*.xls)')
+        filename = QtGui.QFileDialog.getOpenFileName(None, 'Excel', '../', 'Excel File (*.xls)')
         if filename:
             excelparse.parseExcel(filename, self.excel_table)
 
     def excel_gen_invoice_btn_clicked(self):
         excel_table = self.excel_table
-        rowCount = excel_table.rowCount()
-        colCount = excel_table.columnCount()
+        row_count = excel_table.rowCount()
+        col_count = excel_table.columnCount()
 
         # TODO 判断表格中是否有数据
-        if rowCount <= 1:
-            QMessageBox.information(self, "Information", u'请先导入发票数据！')
+        if row_count <= 1:
+            QMessageBox.information(None, "Information", u'请先导入发票数据！')
             return
 
-        for i in range(rowCount):
+        for i in range(row_count):
             tbl_custom_name = tableUtil.qStringToString(excel_table.item(i, 0).text())
             tbl_invoice_invoice_num = tableUtil.qStringToString(excel_table.item(i, 1).text())
             tbl_invoice_total_not_tax = tableUtil.qStringToString(excel_table.item(i, 2).text())
@@ -201,7 +200,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print tbl_invoice_remark
             print "-------------------------------"
 
-        QMessageBox.information(self, "Information", u'数据已经保存到临时数据区！')
+        QMessageBox.information(None, "Information", u'数据已经保存到临时数据区！')
         # TODO 导入成功之后清空数据
 
     def invoice_filter_btn_clicked(self):
@@ -213,15 +212,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         invoice_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
 
         # 查询数据
-        invoiceList = list(Invoice.select().where(Invoice.status==0))
+        invoice_list = list(Invoice.select().where(Invoice.status==0))
 
 
-        rowCount = len(invoiceList)
+        rowCount = len(invoice_list)
         self.invoice_table.setRowCount(rowCount)
 
         # 将数据加载到表格中
         for i in range(rowCount):
-            invoice = invoiceList[i]
+            invoice = invoice_list[i]
 
             tableUtil.setTableItemValue(invoice_table, i, 0, invoice.id)
             tableUtil.setTableItemValue(invoice_table, i, 1, invoice.invoice_num)
@@ -242,14 +241,14 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             tableUtil.setTableItemValue(invoice_table, i, 13, invoice.beneficiary)
             tableUtil.setTableItemValue(invoice_table, i, 14, invoice.reviewer)
 
-    def invoince_update_btn_clicked(self):
+    def invoice_update_btn_clicked(self):
         dialog = FormInvoiceDialog(self)
         dialog.show()
 
 
         pass
 
-    def invoince_delete_btn_clicked(self):
+    def invoice_delete_btn_clicked(self):
             reply = QMessageBox.question(self,u'提示',u'确定要删除所选记录吗？',QMessageBox.Yes|QMessageBox.No)
             if reply == QMessageBox.Yes:
                 invoice_table = self.invoice_table
@@ -258,21 +257,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 idList = []
                 remove_rows = tableUtil.getSelectedRows(invoice_table)
                 for rowCount in remove_rows:
-                    invoince_id = tableUtil.qStringToString(invoice_table.item(rowCount, 0).text())
-                    if invoince_id:
-                        q = Invoice.update(status=-1).where(Invoice.id == invoince_id)
+                    invoice_id = tableUtil.qStringToString(invoice_table.item(rowCount, 0).text())
+                    if invoice_id:
+                        q = Invoice.update(status=-1).where(Invoice.id == invoice_id)
                         q.execute()
 
                 # 重新加载表格
                 self.invoice_filter_btn_clicked()
 
-    def invoince_import_xml_btn_clicked(self):
-        invoinceList = list(Invoice.select(Invoice.status == 0))
-        isSuccess = ExportAsXML.exportAsFile(invoinceList, "1.xml")
+    def invoice_import_xml_btn_clicked(self):
+        invoiceList = list(Invoice.select(Invoice.status == 0))
+        print invoiceList
+        isSuccess = ExportAsXML.exportAsFile(invoiceList, "1.xml")
         if isSuccess:
-            QMessageBox.information(self, "Information", u'导入成功！')
+            QMessageBox.information(None, "Information", u'导入成功！')
         else:
-            QMessageBox.information(self, "Information", u'导入失败，请重试！')
+            QMessageBox.information(None, "Information", u'导入失败，请重试！')
 
     def invoice_table_item_clicked(self, item):
         invoice_detail_table = self.invoice_detail_table
