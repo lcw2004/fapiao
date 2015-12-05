056806652377ed0a5493466d39089fd79d3429d7
Lcw <lcw2004@163.com>
2015-12-05 19:53:10 +0800

    完成登录时候的保存密码功能

---
+++ src/invoice/common/settings.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import pickle
+import os.path
+
+from PyQt4.QtCore import QSettings
+
+class Settings:
+    AUTO_LOGIN = 'auto_login'
+    SAVE_PASSWORD = "save_password"
+    LOGIN_NAME = 'login_name'
+    PASSWORD = 'password'
+
+    _settings = QSettings(os.path.join(os.path.expanduser("~"), 'invoice_helper.ini'), QSettings.IniFormat)
+
+    @staticmethod
+    def set_boolean(key, val):
+        if val:
+            val = '1'
+        else:
+            val = ''
+        Settings._settings.setValue(key, val)
+
+    @staticmethod
+    def set_python_value(key, val):
+        try:
+            Settings._settings.setValue(key, pickle.dumps(val))
+        except pickle.PickleError:
+            pass
+
+    @staticmethod
+    def python_value(key):
+        try:
+            return pickle.loads(Settings._settings.value(key))
+        except:
+            return None
+
+    @staticmethod
+    def set_value(key, val):
+        Settings._settings.setValue(key, val)
+
+    @staticmethod
+    def value(key):
+        return Settings._settings.value(key)
--- src/invoice/gui/login.py
+++ src/invoice/gui/login.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import logging
-
 from PyQt4 import QtCore
 from PyQt4.QtGui import QDialog
 
+from invoice.bean.beans import User
+from invoice.common import table_util
+from invoice.common.settings import Settings
 from invoice.gui.login_ui import Ui_DialogLogin
 
 logger = logging.getLogger(__name__)
@@ -13,15 +15,47 @@ class LoginDialog(QDialog, Ui_DialogLogin):
     def __init__(self, parent=None):
         super(LoginDialog, self).__init__(parent)
         self.setupUi(self)
+        self.init_from_setting()
 
         self.connect(self.login_login_btn, QtCore.SIGNAL("clicked()"), self.login_login_btn_clicked)
         self.connect(self.login_quit_btn, QtCore.SIGNAL("clicked()"), self.login_quit_btn_clicked)
 
+    def init_from_setting(self):
+        logger.info(u"从Setting中加载配置")
+        if Settings.value(Settings.SAVE_PASSWORD).toBool():
+            self.login_save_password_chk.setChecked(True)
+        if Settings.value(Settings.AUTO_LOGIN).toBool():
+            self.login_auto_login_chk.setChecked(True)
+
+        login_name = Settings.value(Settings.LOGIN_NAME).toString()
+        password = Settings.value(Settings.PASSWORD).toString()
+        self.login_username_edit.setText(login_name)
+        self.login_password_edit.setText(password)
+
     def login_login_btn_clicked(self):
-        self.accept();
+        login_name = table_util.get_edit_text(self.login_username_edit)
+        password = table_util.get_edit_text(self.login_password_edit)
+
+        # 判断登录名密码是否为空
+        if not login_name:
+            self.login_error_label.setText("请输入用户登录名！")
+            return
+        if not password:
+            self.login_error_label.setText("请输入用户密码！")
+            return
+
+        # 判断是否需要保存密码
+        if self.login_save_password_chk.isChecked():
+            Settings.set_value(Settings.LOGIN_NAME, login_name)
+            Settings.set_value(Settings.PASSWORD, password)
+        else:
+            Settings.set_value(Settings.LOGIN_NAME, '')
+            Settings.set_value(Settings.PASSWORD, '')
+        Settings.set_value(Settings.AUTO_LOGIN, self.login_auto_login_chk.isChecked())
+        Settings.set_value(Settings.SAVE_PASSWORD, self.login_save_password_chk.isChecked())
 
 
+        self.accept()
 
     def login_quit_btn_clicked(self):
         pass
-
